3c9fcf90318554e4753b4283913c25c7f22f211d
Mike Dalessio <mike.dalessio@gmail.com>
2020-08-15 13:27:16 -0400

    extract env var expansion into a separate module
    to make it easier to test, see next commit

--- resource/lib/gitter-notification-resource.rb
+++ resource/lib/gitter-notification-resource.rb
@@ -8,12 +8,27 @@ class GitterNotificationResource
   MESSAGE_FILES_PATH = File.expand_path(File.join(File.dirname(__FILE__), "..", "messages"))
   UNKNOWN_STATUS = "unknown"
 
+  module EnvExpander
+    ENV_VARIABLES_REGEX = /\$([a-zA-Z_]+[a-zA-Z0-9_]*)|\$\{([a-zA-Z_]+[a-zA-Z0-9_]*)\}/
+    BUILD_URL_TEMPLATE = "${ATC_EXTERNAL_URL}/teams/${BUILD_TEAM_NAME}/pipelines/${BUILD_PIPELINE_NAME}/jobs/${BUILD_JOB_NAME}/builds/${BUILD_NAME}"
+
+    def self.expand_concourse_env(message)
+      ENV["BUILD_URL"] = EnvExpander.expand_env(BUILD_URL_TEMPLATE)
+      expand_env(message)
+    end
+
+    def self.expand_env(message)
+      message.gsub(ENV_VARIABLES_REGEX) do
+        ENV[$1 || $2] || $&
+      end
+    end
+  end
+
   attr_reader :webhook, :dryrun
 
   def initialize(source = {})
     @webhook = source.fetch("webhook")
     @dryrun = source.fetch("dryrun", false)
-    ENV['BUILD_URL'] = expand_env("${ATC_EXTERNAL_URL}/teams/${BUILD_TEAM_NAME}/pipelines/${BUILD_PIPELINE_NAME}/jobs/${BUILD_JOB_NAME}/builds/${BUILD_NAME}")
   end
 
   def out(params = {})
@@ -22,19 +37,18 @@ class GitterNotificationResource
     end
 
     message = if params.key?(OutParams::MESSAGE)
-                params[OutParams::MESSAGE]
-              elsif params.key?(OutParams::MESSAGE_FILE)
-                File.read(params[OutParams::MESSAGE_FILE])
-              elsif params.key?(OutParams::STATUS)
-                expected_file = File.join(MESSAGE_FILES_PATH, "#{params[OutParams::STATUS]}.md")
-                if File.exist?(expected_file)
-                  File.read(expected_file)
-                else
-                  File.read(File.join(MESSAGE_FILES_PATH, "#{UNKNOWN_STATUS}.md"))
-                end
-              end
-
-    message = expand_env(message)
+        params[OutParams::MESSAGE]
+      elsif params.key?(OutParams::MESSAGE_FILE)
+        File.read(params[OutParams::MESSAGE_FILE])
+      elsif params.key?(OutParams::STATUS)
+        expected_file = File.join(MESSAGE_FILES_PATH, "#{params[OutParams::STATUS]}.md")
+        if File.exist?(expected_file)
+          File.read(expected_file)
+        else
+          File.read(File.join(MESSAGE_FILES_PATH, "#{UNKNOWN_STATUS}.md"))
+        end
+      end
+    message = EnvExpander.expand_concourse_env(message)
 
     metadata = []
 
@@ -46,14 +60,6 @@ class GitterNotificationResource
   end
 
   private
-
-  ENV_VARIABLES_REGEX = /\$([a-zA-Z_]+[a-zA-Z0-9_]*)|\$\{([a-zA-Z_]+[a-zA-Z0-9_]*)\}/
-
-  def expand_env(message)
-    message.gsub(ENV_VARIABLES_REGEX) do
-      ENV[$1||$2] || $&
-    end
-  end
 
   def metadata_name_value_pair(key, value)
     { "name" => key, "value" => value }
