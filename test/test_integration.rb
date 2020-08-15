2e5ea19dc11ef8897f5f64c35680617a7c3ab425
Mike Dalessio <mike.dalessio@gmail.com>
2020-08-15 16:14:28 -0400

    "out" script and integration tests for it

--- test/test_integration.rb
+++ test/test_integration.rb
@@ -1,6 +1,7 @@
 require "helper"
 require "json"
 require "open3"
+require "webrick"
 
 describe "resource scripts" do
   describe "/opt/resource/in" do
@@ -22,6 +23,49 @@ describe "resource scripts" do
   end
 
   describe "/opt/resource/out" do
-    it "does some things"
+    it "makes an HTTP POST to the webhook URL with the message payload" do
+      # set up a dummy web server
+      server = WEBrick::HTTPServer.new(Port: 0) # choose a random port
+      port = server.config[:Port]
+      request = nil
+
+      server.mount_proc "/c0ffeec0ffeecafecafe" do |req, res|
+        request = req.to_s # for later inspection
+        res.body = "OK sure"
+      end
+
+      thread = Thread.new { server.start }
+
+      # make the `out` call
+      input = {
+        "source" => {
+          "webhook" => "http://localhost:#{port}/c0ffeec0ffeecafecafe",
+        },
+        "params" => {
+          "message" => "this is a message",
+        },
+      }.to_json
+      stdout, stderr, status = Open3.capture3("resource/out", stdin_data: input)
+
+      # shut it down gracefully
+      thread.exit
+
+      # make our assertions on the server side
+      assert request
+      assert_includes(request, "message=this+is+a+message")
+
+      # make our assertions on the client side
+      assert status.success?, "script failed, #{stderr}"
+      assert stderr.empty?
+      expected_stdout = {
+        "version" => { "ref" => "none" },
+        "metadata" => [
+          { "name" => "webhook", "value" => "http://localhost:#{port}/c0ffeec0ffeecafecafe" },
+          { "name" => "dryrun", "value" => false },
+          { "name" => "message", "value" => "this is a message" },
+        ],
+      }
+      assert_equal expected_stdout, JSON.parse(stdout)
+    end
   end
 end
