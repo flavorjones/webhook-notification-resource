9709b2002fbc0c38b0d247acca685d33d7217a9e
Mike Dalessio <mike.dalessio@gmail.com>
2020-08-15 13:25:14 -0400

    simplify Dockerfile and remove dependency on bash

--- Dockerfile
+++ Dockerfile
@@ -1,18 +1,5 @@
-#
-#  bundle install gems (which should be in ./vendor, see Rakefile)
-#
-FROM ruby:alpine AS bundler
-RUN apk update
-RUN apk add build-base
+FROM ruby:alpine
 
 COPY . /work
 RUN cd /work && bundle install --local
-
-#
-#  run tests
-#  discard the build-base compiler toolchain to have a smaller image
-#
-FROM ruby:alpine AS test
-COPY --from=bundler /work /work/
-COPY --from=bundler /usr/local/bundle /usr/local/bundle/
-RUN cd /work && bundle check && bundle exec rake test
+RUN cd /work && bundle exec rake test
--- test/test_integration.rb
+++ test/test_integration.rb
@@ -1,5 +1,27 @@
 require "helper"
+require "json"
+require "open3"
 
-describe "/opt/resource/out" do
-  it "does some things"
+describe "resource scripts" do
+  describe "/opt/resource/in" do
+    it "emits a placeholder ref and exits 0" do
+      stdout, stderr, status = Open3.capture3("resource/in", stdin_data: "ignored")
+      assert status.success?, "script failed, #{stderr}"
+      assert stderr.empty?
+      assert_equal({ "version" => { "ref" => "none" } }, JSON.parse(stdout))
+    end
+  end
+
+  describe "/opt/resource/check" do
+    it "emits an empty array and exits 0" do
+      stdout, stderr, status = Open3.capture3("resource/check", stdin_data: "ignored")
+      assert status.success?, "script failed, #{stderr}"
+      assert stderr.empty?
+      assert_equal([], JSON.parse(stdout))
+    end
+  end
+
+  describe "/opt/resource/out" do
+    it "does some things"
+  end
 end
