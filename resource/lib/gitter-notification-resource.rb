306545724f7af4bd73592ada77b2b0a55ad475b4
Mike Dalessio <mike.dalessio@gmail.com>
2020-08-15 02:11:18 -0400

    fleshing out the resource behavior around messages

--- resource/lib/gitter-notification-resource.rb
+++ resource/lib/gitter-notification-resource.rb
@@ -1,10 +1,52 @@
 require "rest-client"
 
 class GitterNotificationResource
+  module OutParams
+    STATUS = "status"
+    MESSAGE = "message"
+    MESSAGE_FILE = "message_file"
+  end
+
+  MESSAGE_FILES_PATH = File.expand_path(File.join(File.dirname(__FILE__), "..", "messages"))
+  UNKNOWN_STATUS = "unknown"
+
   attr_reader :webhook, :dryrun
 
   def initialize(source = {})
     @webhook = source.fetch("webhook")
     @dryrun = source.fetch("dryrun", false)
+  end
+
+  def out(params = {})
+    if !params.key?(OutParams::STATUS) && !params.key?(OutParams::MESSAGE) && !params.key?(OutParams::MESSAGE_FILE)
+      raise KeyError.new("could not find 'status', 'message', or 'message_file'")
+    end
+
+    message = if params.key?(OutParams::MESSAGE)
+                params[OutParams::MESSAGE]
+              elsif params.key?(OutParams::MESSAGE_FILE)
+                File.read(params[OutParams::MESSAGE_FILE])
+              elsif params.key?(OutParams::STATUS)
+                expected_file = File.join(MESSAGE_FILES_PATH, "#{params[OutParams::STATUS]}.md")
+                if File.exist?(expected_file)
+                  File.read(expected_file)
+                else
+                  File.read(File.join(MESSAGE_FILES_PATH, "#{UNKNOWN_STATUS}.md"))
+                end
+              end
+
+    metadata = []
+
+    metadata << metadata_name_value_pair("webhook", webhook)
+    metadata << metadata_name_value_pair("dryrun", dryrun)
+    metadata << metadata_name_value_pair("message", message)
+
+    { "version" => { "ref" => "none" }, "metadata" => metadata }
+  end
+
+  private
+
+  def metadata_name_value_pair(key, value)
+    { "name" => key, "value" => value }
   end
 end
--- test/test_unit.rb
+++ test/test_unit.rb
@@ -3,8 +3,8 @@ require "helper"
 describe "GitterNotificationResource" do
   describe "#initialize" do
     it "requires a webhook hash key" do
-      e = assert_raises { GitterNotificationResource.new }
-      assert_match(/key not found.*webhook/, e.to_s)
+      e = assert_raises(KeyError) { GitterNotificationResource.new }
+      assert_match(/webhook/, e.to_s)
 
       GitterNotificationResource.new("webhook" => "https://webhooks.gitter.im/e/c0ffeec0ffeecafecafe")
     end
@@ -31,6 +31,100 @@ describe "GitterNotificationResource" do
       resource = GitterNotificationResource.new("webhook" => "https://webhooks.gitter.im/e/c0ffeec0ffeecafecafe",
                                                 "dryrun" => false)
       refute resource.dryrun
+    end
+  end
+
+  describe "#out" do
+    let(:resource) {
+      GitterNotificationResource.new("webhook" => "https://webhooks.gitter.im/e/c0ffeec0ffeecafecafe",
+                                     "dryrun" => true)
+    }
+    let(:absolute_message_file_path) { File.expand_path(File.join(File.dirname(__FILE__), "test-message.md")) }
+    let(:relative_message_file_path) { "test/test-message.md" } # relative to project root
+
+    it "requires one of 'status', 'message', or 'message_file'" do
+      assert_raises(KeyError) { resource.out }
+      assert_raises(KeyError) { resource.out({}) }
+
+      resource.out("status" => "success")
+      resource.out("message" => "this is a markdown message")
+      resource.out("message_file" => absolute_message_file_path)
+    end
+
+    describe "return value is a hash" do
+      it "contains a placeholder version" do
+        output = resource.out("status" => "success")
+        assert_equal({ "ref" => "none" }, output["version"])
+      end
+
+      it "contains descriptive metadata for source and params" do
+        output = resource.out("message" => "this is a markdown message")
+        assert_includes(output["metadata"], { "name" => "dryrun",
+                                              "value" => true })
+        assert_includes(output["metadata"], { "name" => "webhook",
+                                              "value" => "https://webhooks.gitter.im/e/c0ffeec0ffeecafecafe" })
+        assert_includes(output["metadata"], { "name" => "message",
+                                              "value" => "this is a markdown message" })
+      end
+    end
+
+    describe "when passing 'message_file'" do
+      describe "and the file exists at that absolute path" do
+        it "sets the message to the file contents" do
+          output = resource.out("message_file" => absolute_message_file_path)
+          assert_includes(output["metadata"], { "name" => "message",
+                                                "value" => "this is a markdown message from a file\n" })
+        end
+      end
+
+      describe "and the file exists at that relative path" do
+        it "sets the message to the file contents" do
+          output = resource.out("message_file" => relative_message_file_path)
+          assert_includes(output["metadata"], { "name" => "message",
+                                                "value" => "this is a markdown message from a file\n" })
+        end
+      end
+
+      describe "and the file does not exist" do
+        it "raises an exception" do
+          assert_raises { resource.out("message_file" => "road/to/nowhere") }
+        end
+      end
+    end
+
+    describe "when passing 'status'" do
+      let(:message_file_contents) { File.read(message_file_path) }
+
+      ["success", "failure", "error", "abort"].each do |status|
+        describe "and the status is '#{status}'" do
+          let(:message_file_path) {
+            File.expand_path(File.join(File.basename(__FILE__), "..", "resource", "messages", "#{status}.md"))
+          }
+
+          it "returns the '#{status}' message" do
+            output = resource.out("status" => status)
+            actual = output["metadata"].find { |datum| datum["name"] == "message" }
+            assert_equal message_file_contents, actual["value"]
+          end
+        end
+      end
+
+      describe "and the status is invalid" do
+        let(:message_file_path) {
+          File.expand_path(File.join(File.basename(__FILE__), "..", "resource", "messages", "unknown.md"))
+        }
+        it "returns the 'unknown' message" do
+          output = resource.out("status" => "not-a-valid-status")
+          actual = output["metadata"].find { |datum| datum["name"] == "message" }
+          assert_equal message_file_contents, actual["value"]
+        end
+      end
+    end
+
+    describe "concourse metadata" do
+      # see https://gist.github.com/steakknife/4606598 for some ideas
+      it "expands standard concourse metadata in the message"
+      it "expands custom BUILD_URL metadata in the message"
     end
   end
 end
